424600adc9a639256db2b624d7f919e7930f254a
github username <github email address>
2020-01-20 21:38:12 -0500

    initial js

--- festivalz_backend/app/controllers/artists_controller.rb
+++ festivalz_backend/app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
 
     def index
         @artists = Artist.all
-        render :json => @artists, include: [:performances, :follows]
+        render json: @artists
+        # , include: [:performances, :follows]
     end
 end
--- festivalz_frontend/myscripts.js
+++ festivalz_frontend/myscripts.js
@@ -1,4 +1,35 @@
-document.addEventListener('DOMContentLoaded',function(){
 
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    random(event)
 })
+
+
+
+function random(event) {
+    event.preventDefault()
+
+ fetch("http://localhost:3000/artists")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(function(artist) {
+            let spotify = artist.spotify
+            let playlist = document.querySelector('.playlist')
+            playlist.innerHTML = spotify
+            let card = document.querySelector('.list')
+            let name = document.createElement('h1')
+            name.innerHTML = artist.name
+            let img = document.createElement('img')
+            img.src = artist.image
+            let bio = document.createElement('p')
+            bio.innerHTML = artist.bio
+            card.appendChild(name)
+            card.appendChild(img)
+            card.appendChild(bio)
+
+            
+            debugger
+
+       })
+    }
+    )
+}
