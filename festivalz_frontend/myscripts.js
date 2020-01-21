df010f5d09de8882edec2d3a9e464c2126110d38
github username <github email address>
2020-01-20 23:42:14 -0500

    styling

--- festivalz_frontend/myscripts.js
+++ festivalz_frontend/myscripts.js
@@ -12,24 +12,56 @@ function random(event) {
     .then(res => res.json())
     .then(data => {
         data.forEach(function(artist) {
-            let spotify = artist.spotify
-            let playlist = document.querySelector('.playlist')
-            playlist.innerHTML = spotify
-            let card = document.querySelector('.list')
+            let artistCard = document.createElement('div')
+            artistCard.classList.add("uk-card")
+            artistCard.classList.add("uk-card-default")
+            artistCard.classList.add("uk-grid-small")
+
+
+            let artistMedia = document.createElement('div')
+            artistMedia.classList.add("uk-card-media-top")
+            artistMedia.classList.add("uk-parallax")
+            artistMedia.classList.add("uk-align-center")
+
+
+            let artistBody = document.createElement('div')
+            artistBody.classList.add("uk-card-body")
+
+
             let name = document.createElement('h1')
             name.innerHTML = artist.name
+
             let img = document.createElement('img')
             img.src = artist.image
+            img.width = 600;
+            
+            artistMedia.appendChild(img)
+
+ 
             let bio = document.createElement('p')
             bio.innerHTML = artist.bio
-            card.appendChild(name)
-            card.appendChild(img)
-            card.appendChild(bio)
+
+            let iframe = document.createElement('iframe')
+            let spotify = artist.spotify
+            iframe.src = spotify
+            
+            let allArtists = document.querySelector('.artists')
 
             
-            debugger
+            artistBody.appendChild(name)
+            artistBody.appendChild(bio)
+            // artistBody.appendChild(iframe)
+
+            artistMedia.appendChild(img)
+
+            artistCard.appendChild(artistMedia)
+            artistCard.appendChild(artistBody)
+            artistCard.style.marginBottom = "50px"
+            allArtists.appendChild(artistCard)
+
 
        })
+       debugger
     }
     )
 }
